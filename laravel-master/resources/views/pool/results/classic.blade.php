612e201720a78ee9d72afc1504c663afd24a264c
1333985 <1333985@LQ3505-10.cegeplimoilou.lan>
2016-05-24 17:52:54 -0400

    Page de pool (non inscrit)

--- laravel-master/resources/views/pool/results/classic.blade.php
+++ laravel-master/resources/views/pool/results/classic.blade.php
@@ -14,7 +14,7 @@ $typePool = trans ( 'pagination.poolClassic' );
 		<br />
 		<div class="form-group">
 			<div class="col-md-12">
-				<select id="#selectCoulr" class="form-control" name="pool">
+				<select id="selectPool" class="form-control" name="pool">
 					<option selected disabled>{{ trans('pool.select_pool') }}</option>
 					<option id="red" value="red">{{ trans('param.rouge') }}</option>
 					<option id="cyan" value="cyan">{{ trans('param.cyan') }}</option>
@@ -31,30 +31,106 @@ $typePool = trans ( 'pagination.poolClassic' );
 		</div>
 	</div>
 	<div class="milieu">
-		<div class="table-responsive">
+		<div class="table-responsive col-md-6">
 			<table class="table">
 				<thead>
 					<tr>
-						<th>#</th>
-						<th>Firstname</th>
-						<th>Lastname</th>
-						<th>Age</th>
-						<th>City</th>
-						<th>Country</th>
+						<th>{{ trans('general.user') }}</th>
+						<th>{{ trans('pool.label_score') }}</th>
+						<th>{{ trans('pool.rank') }}</th>
 					</tr>
 				</thead>
 				<tbody>
 					<tr>
+						<td>Samuel Foisy</td>
+						<td>132</td>
 						<td>1</td>
+					</tr>
+					<tr>
+						<td>Olivier Parent</td>
+						<td>100</td>
+						<td>2</td>
+					</tr>
+					<tr>
+						<td>Samuel Dansereau</td>
+						<td>85</td>
+						<td>3</td>
+					</tr>
+					<tr>
+						<td>Simon Côté</td>
+						<td>67</td>
+						<td>4</td>
+					</tr>
+					<tr>
+						<td>Robert Aubé</td>
+						<td>75</td>
+						<td>5</td>
+					</tr>
+					<tr>
+						<td>Bob</td>
+						<td>56</td>
+						<td>6</td>
+					</tr>
+					<tr>
+						<td>Bobinette</td>
+						<td>27</td>
+						<td>7</td>
+					</tr>
+					<tr>
 						<td>Anna</td>
-						<td>Pitt</td>
-						<td>35</td>
-						<td>New York</td>
-						<td>USA</td>
+						<td>22</td>
+						<td>8</td>
+					</tr>
+					<tr>
+						<td>Roger</td>
+						<td>21</td>
+						<td>9</td>
+					</tr>
+					<tr>
+						<td>Robert</td>
+						<td>10</td>
+						<td>10</td>
+					</tr>
+					<tr>
+						<td>Richard</td>
+						<td>2</td>
+						<td>11</td>
 					</tr>
 				</tbody>
 			</table>
 		</div>
+		<div class="parties col-md-6">
+			<div class="precedente">
+			<h2>Parties prédedentes</h2>
+				<div class="images">
+					<div class="image col-md-2">
+						<img src="{{ Auth::user()->getImage() }}" onerror="this.src='{{{ asset('images/profile.png') }}}'" alt="image" class="imgD">
+					</div>
+					<div class="image col-md-2">
+						<img src="{{ Auth::user()->getImage() }}" onerror="this.src='{{{ asset('images/profile.png') }}}'" alt="image" class="imgG">
+					</div>
+				</div>
+			</div>
+			<div class="suivante">
+			<h2>Parties suivantes</h2>
+				<div class="images">
+					<div class="image col-md-2">
+						<img src="{{ Auth::user()->getImage() }}" onerror="this.src='{{{ asset('images/profile.png') }}}'" alt="image" class="imgD">
+					</div>
+					<div class="image col-md-2">
+						<img src="{{ Auth::user()->getImage() }}" onerror="this.src='{{{ asset('images/profile.png') }}}'" alt="image" class="imgG">
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
+
+<script type="text/javascript">
+$(document).ready( function() {
+	$('#selectPool').change(function() {
+		location.href = '#';
+	});
+});
+</script>
 @endsection
