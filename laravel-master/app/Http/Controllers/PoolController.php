cc1f693f4655b850a8131dad8f92785c74bf1771
Olivier <Olivier@10.33.0.59>
2016-06-01 09:04:27 -0400

    Ajout de fichiers pour les pools. Le tout est fonctionel (pas parfait)

--- laravel-master/app/Http/Controllers/PoolController.php
+++ laravel-master/app/Http/Controllers/PoolController.php
@@ -12,7 +12,7 @@ use Illuminate\Routing\Controller as BaseController;
 
 class PoolController extends BaseController {
 	
-	private function obtenUtilsPool (int $pool)
+	private function obtenUtilsPool ($pool)
 	{
 		return DB::table ( 'utilisateur_uti' )
 					->select ( 'UTI_SEQNC', 'UTI_NOM', 'UTI_PRENM' )
@@ -22,7 +22,7 @@ class PoolController extends BaseController {
 					->get ();
 	}
 	
-	private function obtenPartiesPoolUtils (int $utils, int $pool)
+	private function obtenPartiesPoolUtils ($utils, $pool)
 	{
 		return DB::table ( 'partie_equipe_peq' )
 					->join ( 'vote_vot', 'vot_peq_seqnc', '=', 'peq_seqnc' )
@@ -32,14 +32,48 @@ class PoolController extends BaseController {
 					->get();
 	}
 	
-	private function obtenPointsVoteClasq (int $utils, int $pool, int $partie)
+	
+	function sortBySubValue($array, $value, $asc = true, $preserveKeys = false)
 	{
-		return DB::select('select case 
-								  when indic_win = \'O\' 
+		if ($preserveKeys) {
+			$c = array();
+			if (is_object(reset($array))) {
+				foreach ($array as $k => $v) {
+					$b[$k] = strtolower($v->$value);
+				}
+			} else {
+				foreach ($array as $k => $v) {
+					$b[$k] = strtolower($v[$value]);
+				}
+			}
+			$asc ? asort($b) : arsort($b);
+			foreach ($b as $k => $v) {
+				$c[$k] = $array[$k];
+			}
+			$array = $c;
+		} else {
+			if (is_object(reset($array))) {
+				usort($array, function ($a, $b) use ($value, $asc) {
+					return $a->{$value} == $b->{$value} ? 0 : ($a->{$value} - $b->{$value}) * ($asc ? 1 : -1);
+				});
+			} else {
+				usort($array, function ($a, $b) use ($value, $asc) {
+					return $a[$value] == $b[$value] ? 0 : ($a[$value] - $b[$value]) * ($asc ? 1 : -1);
+				});
+			}
+		}
+		return $array;
+	}
+	
+	/*****************************************************************/
+	private function obtenPointsVoteClasq ($utils, $pool, $partie)
+	{
+		return DB::select('select case
+								  when indic_win = \'O\'
 							      and ((peq_indic_home = \'O\' and par_cote < 0)
 										or (peq_indic_home = \'N\' and par_cote > 0)) then
 								   1.5
-								 when indic_win = \'O\' 
+								 when indic_win = \'O\'
 							      and ((peq_indic_home = \'O\' and par_cote > 0)
 										or (peq_indic_home = \'N\' and par_cote < 0)) then
 								1
@@ -76,53 +110,21 @@ class PoolController extends BaseController {
 							   and vot_poo_seqnc = ?',
 				[$partie,$partie,$partie,$partie,$partie,$partie,$utils,$pool,])[0]->score;
 	}
-	
+
 	private function obtenScorePoolClasq ($utils, $pool)
 	{
 		$parties = $this::obtenPartiesPoolUtils($utils, $pool);
 		$score = 0;
-		
+	
 		foreach ($parties as $partie)
 		{
 			$score += $this::obtenPointsVoteClasq ($utils, $pool, $partie->PEQ_PAR_SEQNC);
 		}
-		
+	
 		return $score;
 	}
 	
-	function sortBySubValue($array, $value, $asc = true, $preserveKeys = false)
-	{
-		if ($preserveKeys) {
-			$c = array();
-			if (is_object(reset($array))) {
-				foreach ($array as $k => $v) {
-					$b[$k] = strtolower($v->$value);
-				}
-			} else {
-				foreach ($array as $k => $v) {
-					$b[$k] = strtolower($v[$value]);
-				}
-			}
-			$asc ? asort($b) : arsort($b);
-			foreach ($b as $k => $v) {
-				$c[$k] = $array[$k];
-			}
-			$array = $c;
-		} else {
-			if (is_object(reset($array))) {
-				usort($array, function ($a, $b) use ($value, $asc) {
-					return $a->{$value} == $b->{$value} ? 0 : ($a->{$value} - $b->{$value}) * ($asc ? 1 : -1);
-				});
-			} else {
-				usort($array, function ($a, $b) use ($value, $asc) {
-					return $a[$value] == $b[$value] ? 0 : ($a[$value] - $b[$value]) * ($asc ? 1 : -1);
-				});
-			}
-		}
-		return $array;
-	}
-	
-	private function obtenStatsPoolClasq (int $pool)
+	private function obtenStatsPoolClasq ($pool)
 	{
 		$stats = array();
 		$users = $this::obtenUtilsPool ($pool);
@@ -162,22 +164,9 @@ class PoolController extends BaseController {
 		return $stats;
 	}
 	
-	private function estParticipant ($utils, $pool)
-	{
-		return 0 < DB::table ( 'utilisateur_pool_utp' )
-							->select ('UTP_SEQNC')
-							->where ('UTP_POO_SEQNC', $pool)
-							->where ('UTP_UTI_SEQNC', $utils)
-							->count();;
-	}
-	
 	public function getPoolClassic(Request $request) 
 	{
-		
 		$courn = $request ['poolCourant'];
-		$scoreCourn = 0;
-		$rangCourn = 0;
-		
 		$stats = array();
 		
 		$pools = DB::table ( 'pool_poo' )
@@ -192,15 +181,6 @@ class PoolController extends BaseController {
 		if ($courn != null)
 		{
 			$stats = $this::obtenStatsPoolClasq($courn);
-		}
-		 
-		foreach ($stats as $stat)
-		{
-			if($stat['utils'] == Auth::user()->UTI_SEQNC)
-			{
-				$scoreCourn = $stat['score'];
-				$rangCourn = $stat['rang'];
-			}
 		}
 		
 		$partie_suivt = DB::table ( 'partie_par' )
@@ -224,31 +204,316 @@ class PoolController extends BaseController {
 							->orderBy('par_date', 'desc')
 							->take (1)
 							->get();
-			
-		if ($this::estParticipant(Auth::user()->UTI_SEQNC, $courn))
+							
+		return View::make ( '/pool/classic/non-inscrit', array (
+				'pools' => $pools,
+				'poolCourant' => $courn,
+				'scores' => $stats,
+				'partie_precd' => $this->getImagesPartie($partie_precd),
+				'partie_suivt' => $this->getImagesPartie($partie_suivt),
+		) );
+	}
+	
+	
+	/*****************************************************************/
+	private function obtenPointsVotePlayf ($utils, $pool, $partie)
+	{
+		return DB::select('select case
+								  when indic_win = \'O\'
+							      and ((peq_indic_home = \'O\' and par_cote < 0)
+										or (peq_indic_home = \'N\' and par_cote > 0)) then
+								   1.5
+								 when indic_win = \'O\'
+							      and ((peq_indic_home = \'O\' and par_cote > 0)
+										or (peq_indic_home = \'N\' and par_cote < 0)) then
+								1
+								 when indic_win = \'N\'
+									and ((peq_indic_home = \'O\' and par_cote > 0)
+									    or (peq_indic_home = \'N\' and par_cote < 0))
+									and (dif_valr < par_cote)then
+							         0.5
+								   else
+									 0
+								   end
+							         score
+							  from vote_vot,
+								   partie_par,
+								   (select peq_seqnc, peq_indic_home, \'O\' indic_win
+								     from partie_equipe_peq
+								    where PEQ_PAR_SEQNC = ?
+								      and peq_score = (select max(peq_score)
+								     from partie_equipe_peq
+								    where PEQ_PAR_SEQNC = ?)
+								   union
+								   select peq_seqnc, peq_indic_home, \'N\' indic_win
+							  	     from partie_equipe_peq
+								    where PEQ_PAR_SEQNC = ?
+							 	      and peq_score = (select min(peq_score)
+								     from partie_equipe_peq
+								    where PEQ_PAR_SEQNC = ?)) reslt,
+								   (select max(peq_score) - min(peq_score) dif_valr
+							  	      from partie_equipe_peq
+								     where PEQ_PAR_SEQNC = ?) dif
+							 where peq_seqnc = vot_peq_seqnc
+							   and par_seqnc = ?
+							   and vot_uti_seqnc = ?
+							   and vot_poo_seqnc = ?',
+				[$partie,$partie,$partie,$partie,$partie,$partie,$utils,$pool,])[0]->score;
+	}
+	
+	private function obtenScorePoolPlayf ($utils, $pool)
+	{
+		$parties = $this::obtenPartiesPoolUtils($utils, $pool);
+		$score = 0;
+	
+		foreach ($parties as $partie)
 		{
-			return View::make ( '/pool/classic/inscrit', array (
-					'pools' => $pools,
-					'poolCourant' => $courn,
-					'scores' => $stats,
-					'partie_precd' => $this->getImagesPartie($partie_precd),
-					'partie_suivt' => $this->getImagesPartie($partie_suivt),	
-					'scoreCourn' => $scoreCourn,
-					'rangCourn' => $rangCourn,
-			) );
+			$score += $this::obtenPointsVoteClasq ($utils, $pool, $partie->PEQ_PAR_SEQNC);
+		}
+	
+		return $score;
+	}
+	
+	private function obtenStatsPoolPlayf ($pool)
+	{
+		$stats = array();
+		$users = $this::obtenUtilsPool ($pool);
+			
+		foreach ($users as $user )
+		{
+			$stat = array ("utils" => $user->UTI_SEQNC,
+					"nom" => $user->UTI_PRENM . ' ' . $user->UTI_NOM,
+					"score" => $this::obtenScorePoolPlayf($user->UTI_SEQNC, $pool),
+					"rang" => 1,
+			);
+			array_push ($stats, $stat);
+		}
+			
+		$stats = $this::sortBySubValue($stats, "score");
+			
+		if ((count ($stats) < 2) || $stats[0]["score"] != $stats[1]["score"])
+		{
+			$rang = 0;
 		}
 		else
 		{
-			return View::make ( '/pool/classic/non-inscrit', array (
-					'pools' => $pools,
-					'poolCourant' => $courn,
-					'scores' => $stats,
-					'partie_precd' => $this->getImagesPartie($partie_precd),
-					'partie_suivt' => $this->getImagesPartie($partie_suivt),
-			) );
+			$rang = 1;
 		}
-				
+			
+		$precd = -1;
+		for($i = 0; $i < count($stats); $i++) {
+			if ($stats[$i]["score"] != $precd)
+			{
+				$rang++;
+			}
+	
+			$stats[$i]["rang"] = $rang;
+			$precd = $stats[$i]["score"];
+		}
+	
+		return $stats;
 	}
+	
+	public function getPoolPlayoff(Request $request)
+	{
+		$courn = $request ['poolCourant'];
+		$stats = array();
+	
+		$pools = DB::table ( 'pool_poo' )
+		->join ('type_pool_typ', 'typ_seqnc', '=', 'poo_typ_seqnc')
+		->select ( 'POO_SEQNC', 'POO_NOM' )
+		->where ('typ_nom', 'poolPlayoff')
+		->get ();
+			
+		if ($courn == null and isset($pools[0])) {
+			$courn = $pools [0]->POO_SEQNC;
+		}
+		if ($courn != null)
+		{
+			$stats = $this::obtenStatsPoolPlayf($courn);
+		}
+	
+		$partie_suivt = DB::table ( 'partie_par' )
+		->join ( 'semaine_sem', 'sem_seqnc', '=', 'par_sem_seqnc' )
+		->join ( 'saison_sai', 'sai_seqnc', '=', 'sem_sai_seqnc' )
+		->join ( 'pool_poo', 'sai_seqnc', '=', 'poo_sai_seqnc' )
+		->select ('PAR_SEQNC')
+		->whereRaw ('par_date > sysdate()')
+		->where ('poo_seqnc', $courn)
+		->orderBy('par_date')
+		->take (1)
+		->get();
+	
+		$partie_precd = DB::table ( 'partie_par' )
+		->join ( 'semaine_sem', 'sem_seqnc', '=', 'par_sem_seqnc' )
+		->join ( 'saison_sai', 'sai_seqnc', '=', 'sem_sai_seqnc' )
+		->join ( 'pool_poo', 'sai_seqnc', '=', 'poo_sai_seqnc' )
+		->select ('PAR_SEQNC')
+		->whereRaw ('par_date < sysdate()')
+		->where ('poo_seqnc', $courn)
+		->orderBy('par_date', 'desc')
+		->take (1)
+		->get();
+			
+		return View::make ( '/pool/playoff/non-inscrit', array (
+				'pools' => $pools,
+				'poolCourant' => $courn,
+				'scores' => $stats,
+				'partie_precd' => $this->getImagesPartie($partie_precd),
+				'partie_suivt' => $this->getImagesPartie($partie_suivt),
+		) );
+	}
+	
+	/*****************************************************************/
+	private function obtenPointsVoteSurvr ($utils, $pool, $partie)
+	{
+		return DB::select('select case
+								  when indic_win = \'O\'
+							      and ((peq_indic_home = \'O\' and par_cote < 0)
+										or (peq_indic_home = \'N\' and par_cote > 0)) then
+								   1.5
+								 when indic_win = \'O\'
+							      and ((peq_indic_home = \'O\' and par_cote > 0)
+										or (peq_indic_home = \'N\' and par_cote < 0)) then
+								1
+								 when indic_win = \'N\'
+									and ((peq_indic_home = \'O\' and par_cote > 0)
+									    or (peq_indic_home = \'N\' and par_cote < 0))
+									and (dif_valr < par_cote)then
+							         0.5
+								   else
+									 0
+								   end
+							         score
+							  from vote_vot,
+								   partie_par,
+								   (select peq_seqnc, peq_indic_home, \'O\' indic_win
+								     from partie_equipe_peq
+								    where PEQ_PAR_SEQNC = ?
+								      and peq_score = (select max(peq_score)
+								     from partie_equipe_peq
+								    where PEQ_PAR_SEQNC = ?)
+								   union
+								   select peq_seqnc, peq_indic_home, \'N\' indic_win
+							  	     from partie_equipe_peq
+								    where PEQ_PAR_SEQNC = ?
+							 	      and peq_score = (select min(peq_score)
+								     from partie_equipe_peq
+								    where PEQ_PAR_SEQNC = ?)) reslt,
+								   (select max(peq_score) - min(peq_score) dif_valr
+							  	      from partie_equipe_peq
+								     where PEQ_PAR_SEQNC = ?) dif
+							 where peq_seqnc = vot_peq_seqnc
+							   and par_seqnc = ?
+							   and vot_uti_seqnc = ?
+							   and vot_poo_seqnc = ?',
+				[$partie,$partie,$partie,$partie,$partie,$partie,$utils,$pool,])[0]->score;
+	}
+
+	private function obtenScorePoolSurvr ($utils, $pool)
+	{
+		$parties = $this::obtenPartiesPoolUtils($utils, $pool);
+		$score = 0;
+	
+		foreach ($parties as $partie)
+		{
+			$score += $this::obtenPointsVoteSurvr ($utils, $pool, $partie->PEQ_PAR_SEQNC);
+		}
+	
+		return $score;
+	}
+	
+	private function obtenStatsPoolSurvr ($pool)
+	{
+		$stats = array();
+		$users = $this::obtenUtilsPool ($pool);
+			
+		foreach ($users as $user )
+		{
+			$stat = array ("utils" => $user->UTI_SEQNC,
+					"nom" => $user->UTI_PRENM . ' ' . $user->UTI_NOM,
+					"score" => $this::obtenScorePoolSurvr($user->UTI_SEQNC, $pool),
+					"rang" => 1,
+			);
+			array_push ($stats, $stat);
+		}
+			
+		$stats = $this::sortBySubValue($stats, "score");
+			
+		if ((count ($stats) < 2) || $stats[0]["score"] != $stats[1]["score"])
+		{
+			$rang = 0;
+		}
+		else
+		{
+			$rang = 1;
+		}
+			
+		$precd = -1;
+		for($i = 0; $i < count($stats); $i++) {
+			if ($stats[$i]["score"] != $precd)
+			{
+				$rang++;
+			}
+	
+			$stats[$i]["rang"] = $rang;
+			$precd = $stats[$i]["score"];
+		}
+	
+		return $stats;
+	}
+	
+	public function getPoolSurvivor(Request $request)
+	{
+		$courn = $request ['poolCourant'];
+		$stats = array();
+	
+		$pools = DB::table ( 'pool_poo' )
+		->join ('type_pool_typ', 'typ_seqnc', '=', 'poo_typ_seqnc')
+		->select ( 'POO_SEQNC', 'POO_NOM' )
+		->where ('typ_nom', 'poolSurvivor')
+		->get ();
+			
+		if ($courn == null and isset($pools[0])) {
+			$courn = $pools [0]->POO_SEQNC;
+		}
+		if ($courn != null)
+		{
+			$stats = $this::obtenStatsPoolSurvr($courn);
+		}
+	
+		$partie_suivt = DB::table ( 'partie_par' )
+		->join ( 'semaine_sem', 'sem_seqnc', '=', 'par_sem_seqnc' )
+		->join ( 'saison_sai', 'sai_seqnc', '=', 'sem_sai_seqnc' )
+		->join ( 'pool_poo', 'sai_seqnc', '=', 'poo_sai_seqnc' )
+		->select ('PAR_SEQNC')
+		->whereRaw ('par_date > sysdate()')
+		->where ('poo_seqnc', $courn)
+		->orderBy('par_date')
+		->take (1)
+		->get();
+	
+		$partie_precd = DB::table ( 'partie_par' )
+		->join ( 'semaine_sem', 'sem_seqnc', '=', 'par_sem_seqnc' )
+		->join ( 'saison_sai', 'sai_seqnc', '=', 'sem_sai_seqnc' )
+		->join ( 'pool_poo', 'sai_seqnc', '=', 'poo_sai_seqnc' )
+		->select ('PAR_SEQNC')
+		->whereRaw ('par_date < sysdate()')
+		->where ('poo_seqnc', $courn)
+		->orderBy('par_date', 'desc')
+		->take (1)
+		->get();
+			
+		return View::make ( '/pool/survivor/non-inscrit', array (
+				'pools' => $pools,
+				'poolCourant' => $courn,
+				'scores' => $stats,
+				'partie_precd' => $this->getImagesPartie($partie_precd),
+				'partie_suivt' => $this->getImagesPartie($partie_suivt),
+		) );
+	}
+	
+	/*****************************************************************/
 	
 	public function getImagesPartie($partie)
 	{
---
+++ laravel-master/resources/views/pool/playoff/non-inscrit.blade.php
@@ -0,0 +1,136 @@
+@extends('layouts.app') 
+@section('title') 
+	{{ trans('pagination.poolPlayoff') }} 
+@endsection
+@section('content')
+<link type="text/css" rel="stylesheet" href="{{ URL::asset('css/pool.css') }}"></link>
+<br />
+<div class="container">
+	@if (session('status'))
+	<div class="alert alert-success">{{ session('status') }}</div>
+	@endif
+	@if ($poolCourant == null)
+	<p class="col-md-12">{{ trans('pool.text_aucunPool',['pool' => trans('pagination.poolPlayoff')]) }}</p>
+	@else
+	<div class="form-inline">
+		<p class="col-md-12">{{ trans('pool.text_nonInscr',['pool' => trans('pagination.poolPlayoff')]) }}</p>
+		<br />
+		<div class="form-group">
+			<div class="col-md-12">
+				<select id="selectPool" class="form-control" name="pool">
+					<option disabled>{{ trans('pool.select_pool') }}</option>
+						@foreach($pools as $pool)
+							@if ($pool->POO_SEQNC == $poolCourant)
+					    <option value="{{$pool->POO_SEQNC}}" selected>{{ $pool->POO_NOM }}</option>
+							@else
+					    <option value="{{$pool->POO_SEQNC}}">{{ $pool->POO_NOM }}</option>
+						    @endif
+						@endforeach
+				</select>
+			</div>
+		</div>
+		<div class="form-group form-butn">
+			<div class="col-md-2">
+				<button type="submit" class="butn" onClick="inscrire();">
+					<i class="fa fa-btn fa-plus"></i>{{ trans('pool.butn_inscr') }}
+				</button>
+			</div>
+		</div>
+	</div>
+	<div class="milieu">
+		<div class="table-responsive col-md-6">
+			<table class="table">
+				<thead>
+					<tr>
+						<th>{{ trans('general.user') }}</th>
+						<th>{{ trans('pool.label_score') }}</th>
+						<th>{{ trans('pool.rank') }}</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach($scores as $score)
+						@if ($score["utils"] == Auth::user()->UTI_SEQNC)
+						<tr class="user_courant">
+						@else
+						<tr>
+						@endif
+							<td>{{ $score["nom"] }}</td>
+							<td>{{ $score["score"] }}</td>
+							<td>{{ $score["rang"] }}</td>
+						</tr>
+					@endforeach
+				</tbody>
+			</table>
+		</div>
+		<div class="parties col-md-6">
+			@if ($partie_precd != null)
+			<div class="precedente">
+				<h2>{{ trans ('pool.partiePrec') }}</h2>
+				<div class="images">
+					<div class="image col-md-2">
+						<img src="{{ $partie_precd['image2'] }}" alt="image" class="imgWin">
+					</div>
+					<div class="image col-md-2">
+						<img src="{{ $partie_precd['image1'] }}" alt="image" class="imgLose">
+					</div>
+				</div>
+			</div>
+			@endif
+			@if ($partie_suivt != null)
+			<div class="suivante">
+				<h2>{{ trans ('pool.partieSuiv') }}</h2>
+				<div class="images">
+					<div class="image col-md-2">
+						<img src="{{ $partie_suivt['image2'] }}" alt="image" class="imgEven">
+					</div>
+					<div class="image col-md-2">
+						<img src="{{ $partie_suivt['image1'] }}" alt="image" class="imgEven">
+					</div>
+				</div>
+			</div>
+			@endif
+		</div>
+	</div>
+	@endif
+</div>
+
+<script type="text/javascript">
+var tokenMobile = "{{ csrf_token() }}";
+$(document).ready( function() {
+	$('#selectPool').change(function() {
+		window.location= "{{ url('/poolPlayoff') }}?poolCourant=" + $('#selectPool').val();
+	});
+});
+
+function inscrire() {
+	$.post('inscription', {action: 'send', typePool: 'poolPlayoff', poolCourant: <?=$poolCourant?> , _token:tokenMobile}, function(data){
+		window.location = "{{ url('/poolPlayoff') }}";
+	});
+}
+
+function proceed () {
+    var form = document.createElement('form');
+    form.setAttribute('method', 'post');
+    form.setAttribute('action', "{{ url('/inscription') }}");
+    form.style.display = 'hidden';
+    $('<input>').attr({
+        type: 'hidden',
+        id: 'poolCourant',
+        name: 'poolCourant',
+        value: <?=$poolCourant?>
+    }).appendTo('form');
+
+    $('<input>').attr({
+        type: 'hidden',
+        id: '_token',
+        name: '_token',
+        value: "{{ csrf_token() }}"
+    }).appendTo('form');
+    
+    alert (form);
+    document.body.appendChild(form)
+    
+    form.submit();
+}
+</script>
+@endsection
