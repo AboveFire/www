c86c5fd49a2f09e96aebd43359892e20fd8491b5
Samuel Dansereau <samuda31@hotmail.com>
2016-06-11 15:25:32 -0400

    faute corrigée dans about

--- laravel-master/resources/lang/fr/about.php
+++ laravel-master/resources/lang/fr/about.php
@@ -1,6 +1,6 @@
 <?php
 return [ 
-		'disclaimer' => 'Tous les logos des équipes et leurs nom sont une propriété de la NFL.',
+		'disclaimer' => 'Tous les logos des équipes et leurs noms sont une propriété de la NFL.',
 		'disclaimer2' => 'Toutes les informations proviennent du site officiel de la NFL.',
 		'moreInfo' => 'Pour plus d\'information, contacter Samuel Dansereau par courriel à l\'adresse ',
 		'header' => 'Réalisé par',
