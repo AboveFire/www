491926ce420e51ad54d2e26e78d56dfcf4d50b53
1333985 <1333985@LQ3505-10.cegeplimoilou.lan>
2016-06-14 09:49:51 -0400

    bug

--- laravel-master/resources/lang/en/about.php
+++ laravel-master/resources/lang/en/about.php
@@ -2,7 +2,7 @@
 return [ 
 		'disclaimer' => 'All logos and team names are the property of the NFL.',
 		'disclaimer2' => 'All data comes from the official NFL website.',
-		'moreInfo' => 'For more information, please contact Samuel Dansereau by email at :mail',
+		'moreInfo' => 'For more information, please contact Samuel Dansereau by email at',
 		'header' => 'Created by',
 		'rules' => 'Click here to download the rules (French version only).',
 		'apk' => 'Click here to download the Android APK.',
